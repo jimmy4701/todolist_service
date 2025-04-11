f2220968206f8127e3bd2cfd61a70af8deeb6ee6
Devaxis Engineering <contact@devaxis.fr>
2025-04-11 12:18:51 +0200

    :card_file_box: (DATABASE) runFromDB and initialization of database

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express')
+const sqlite3 = require('sqlite3').verbose();
+
 const app = express()
 const port = 3000
 
+const runFromDB = (sql_request) => {
+    const db = new sqlite3.Database('db.sqlite');
+    const result = db.serialize(() => {
+        return db.run(sql_request);
+    });    
+    db.close() 
+    return result
+}
+
+// Initialisation de la base de données
+runFromDB("CREATE TABLE IF NOT EXISTS Task (id INTEGER PRIMARY KEY ASC, title TEXT, completed TINYINT)");
+
 
 app.get('/', (req, res) => {
-    res.send("Je suis recrutable maintenant !")
+    const tasks = runFromDB("SELECT * FROM Task")
+    res.send(tasks)
 })
 
 app.listen(port, () => {
