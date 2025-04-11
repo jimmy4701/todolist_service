60cf7f51bf3829ba8e16e1b806623e8315d6f969
Devaxis Engineering <contact@devaxis.fr>
2025-04-11 11:32:29 +0200

    :sparkles: (INIT EXPRESS SERVER) Initialized basic endpoint

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("HELLO !!!!!")
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => {
+    res.send("Je suis recrutable maintenant !")
+})
+
+app.listen(port, () => {
+    console.log("✅ Server launched....")
+})
