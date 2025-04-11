bcb022fd60b39d2717ad20a796c8a4a633a3f232
Devaxis Engineering <contact@devaxis.fr>
2025-04-11 12:42:18 +0200

    :sparkles: (POST TASK) Added endpoint to create task

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require('sqlite3').verbose()
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = 3000
+app.use(bodyParser.json())
 
 const runFromDB = (sql_request) => {
     const db = new sqlite3.Database('db.sqlite');
@@ -17,10 +19,17 @@ const runFromDB = (sql_request) => {
 runFromDB("CREATE TABLE IF NOT EXISTS Task (id INTEGER PRIMARY KEY ASC, title TEXT, completed TINYINT)");
 
 
-app.get('/', (req, res) => {
+app.get('/tasks', (req, res) => {
     const tasks = runFromDB("SELECT * FROM Task")
     res.send(tasks)
 })
+
+app.post('/tasks', (req, res) => {
+    const body = req.body
+    console.log(body)
+    runFromDB(`INSERT INTO Task (title, completed) VALUES ("${body.title}", "0")`)
+    res.status(200).send("Task created")
+} )
 
 app.listen(port, () => {
     console.log("✅ Server launched....")
